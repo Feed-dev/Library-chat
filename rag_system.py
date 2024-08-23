b08d13d08402c16d73ced18e586e7c41970968f2
Feed-dev <federico.lubbert@gmail.com>
2024-08-23 13:35:29 +0200

    updated

--- rag_system.py
+++ rag_system.py
@@ -6,16 +6,25 @@ from langchain.embeddings import CohereEmbeddings
 from langchain.llms import Ollama
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
-import config
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+PINECONE_ENVIRONMENT = os.getenv("PINECONE_ENVIRONMENT")
+PINECONE_INDEX_NAME = os.getenv("PINECONE_INDEX_NAME")
+COHERE_API_KEY = os.getenv("COHERE_API_KEY")
+OLLAMA_BASE_URL = os.getenv("OLLAMA_BASE_URL")
 
 def initialize_vector_store():
-    pc = Pinecone(api_key=config.PINECONE_API_KEY)
-    pinecone_index = pc.Index(config.PINECONE_INDEX_NAME)
-    embeddings = CohereEmbeddings(model="embed-multilingual-v3.0", cohere_api_key=config.COHERE_API_KEY)
+    pc = Pinecone(api_key=PINECONE_API_KEY)
+    pinecone_index = pc.Index(PINECONE_INDEX_NAME)
+    embeddings = CohereEmbeddings(model="embed-multilingual-v3.0", cohere_api_key=COHERE_API_KEY)
     return LangchainPinecone(pinecone_index, embeddings, "text")
 
 def initialize_llm():
-    return Ollama(model="dolphin-llama3:8b", base_url=config.OLLAMA_BASE_URL)
+    return Ollama(model="dolphin-llama3:8b", base_url=OLLAMA_BASE_URL)
 
 def create_rag_chain(vector_store, llm):
     prompt_template = """You are a highly capable research assistant. Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
